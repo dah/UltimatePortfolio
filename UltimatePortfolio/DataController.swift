67033f8c76c6a67e993c10f50d711c54b56af88d
Dan Hancu <dan@hancu.org>
2023-02-19 13:08:35 +0000

    Commit After Lesson 2

---
+++ UltimatePortfolio/DataController.swift
@@ -0,0 +1,83 @@
+//
+//  DataController.swift
+//  UltimatePortfolio
+//
+//  Created by Dan Hancu on 19/02/2023.
+//
+
+import CoreData
+
+class DataController:ObservableObject {
+    let container: NSPersistentCloudKitContainer
+    
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentCloudKitContainer(name: "Main")
+        
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(filePath: "/dev/null")
+        }
+        
+        container.loadPersistentStores { storeDescription, error in
+            if let error {
+                fatalError("Fatal error loading store: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    static var preview: DataController = {
+        let dataController = DataController(inMemory: true)
+        dataController.createSampleData()
+        return dataController
+    }()
+    
+    func createSampleData() {
+        let viewContext = container.viewContext
+        
+        for i in 1...5 {
+            let tag = Tag(context: viewContext)
+            tag.id = UUID()
+            tag.name = "Tag \(i)"
+            
+            for j in 1...10 {
+                let issue = Issue(context: viewContext)
+                issue.title = "Issue \(j)"
+                issue.content = "Description goes here"
+                issue.creationDate = Date.now
+                issue.completed = Bool.random()
+                issue.priority = Int16.random(in: 0...2)
+                tag.addToIssues(issue)
+            }
+        }
+    }
+    
+    func save() {
+        if container.viewContext.hasChanges {
+            try? container.viewContext.save()
+        }
+    }
+    
+    func delete(_ object: NSManagedObject) {
+        container.viewContext.delete(object)
+        save()
+    }
+    
+    private func delete(_ fetchRequest: NSFetchRequest<NSFetchRequestResult>) {
+        let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        batchDeleteRequest.resultType = .resultTypeObjectIDs
+        
+        if let delete = try? container.viewContext.execute(batchDeleteRequest) as? NSBatchDeleteResult {
+            let changes = [NSDeletedObjectsKey: delete.result as? [NSManagedObjectID] ?? []]
+            NSManagedObjectContext.mergeChanges(fromRemoteContextSave: changes, into: [container.viewContext])
+        }
+    }
+    
+    func deleteAll() {
+        let request1: NSFetchRequest<NSFetchRequestResult> = Tag.fetchRequest()
+        let request2: NSFetchRequest<NSFetchRequestResult> = Issue.fetchRequest()
+        delete(request1)
+        delete(request2)
+        save()
+    }
+    
+}
